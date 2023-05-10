fe7ebab2e4e050ab2c67befa9c76dc307cd7f072
pro470 <63075278+pro470@users.noreply.github.com>
2023-05-10 11:44:24 +0200

    fix bug now you can download

--- Live_Addon/__init__.py
+++ Live_Addon/__init__.py
@@ -5,6 +5,7 @@ from . import ops
 from . import handlers
 from . import ui
 from . import timer
+from . import utils
 import bpy
 import os
 
@@ -25,6 +26,7 @@ def unregister():
     file_path12 = bpy.context.window_manager.my_addon_props.file_path
     blend_match = r"\.blend"
     matched_blend = re.search(blend_match, file_path12)
-    if os.path.exists(file_path12) and matched_blend:
+    # when it is not a folder and it is a blend file
+    if not os.path.isdir(file_path12) and matched_blend and os.path.exists(file_path12):
         os.remove(file_path12)
     props.unregister()
